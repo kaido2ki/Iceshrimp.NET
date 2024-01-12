7cb1d7a5b696edc52fff8534cc024e12aced8304
Laura Hausmann <laura@hausmann.dev>
2024-01-12 19:16:58 +0100

    Un-stub AcctToTuple

--- Iceshrimp.Backend/Core/Services/UserService.cs
+++ Iceshrimp.Backend/Core/Services/UserService.cs
@@ -7,9 +7,13 @@ namespace Iceshrimp.Backend.Core.Services;
 public class UserService(ILogger<UserService> logger, DatabaseContext db) {
 	private static (string Username, string Host) AcctToTuple(string acct) {
 		if (!acct.StartsWith("acct:")) throw new Exception("Invalid query");
-		throw new NotImplementedException(); //FIXME
+
+		var split = acct[5..].Split('@');
+		if (split.Length != 2) throw new Exception("Invalid query");
+
+		return (split[0], split[1]);
 	}
-	
+
 	public Task<User?> GetUserFromQuery(string query) {
 		if (query.StartsWith("http://") || query.StartsWith("https://")) {
 			return db.Users.FirstOrDefaultAsync(p => p.Uri == query);
@@ -18,7 +22,7 @@ public class UserService(ILogger<UserService> logger, DatabaseContext db) {
 		var tuple = AcctToTuple(query);
 		return db.Users.FirstOrDefaultAsync(p => p.Username == tuple.Username && p.Host == tuple.Host);
 	}
-	
+
 	public async Task<User> CreateUser(string uri, string acct) {
 		throw new NotImplementedException(); //FIXME
 	}
