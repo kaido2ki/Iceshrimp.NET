b4c1d45ac9ddac832743944dde549086b0a30b2a
Laura Hausmann <laura@hausmann.dev>
2024-01-20 18:36:18 +0100

    Cleanup ASLink

--- Iceshrimp.Backend/Core/Federation/ActivityStreams/Types/ASLink.cs
+++ Iceshrimp.Backend/Core/Federation/ActivityStreams/Types/ASLink.cs
@@ -6,11 +6,8 @@ public class ASLink(string url) : LDIdObject(url) {
 	[J("https://www.w3.org/ns/activitystreams#href")]
 	public string? Href { get; set; }
 
-	public string? Link => Id ?? Href;
-
-	public override string? ToString() {
-		return Link;
-	}
+	public          string? Link       => Id ?? Href;
+	public override string? ToString() => Link;
 }
 
 public class ASLinkConverter : ASSerializer.ListSingleObjectConverter<ASLink>;
