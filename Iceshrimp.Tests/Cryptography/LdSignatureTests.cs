9ac55fdeb873d549f0f71c671eb1043fdca89527
Laura Hausmann <laura@hausmann.dev>
2024-01-20 21:22:08 +0100

    Add more test cases for LD signatures

--- Iceshrimp.Tests/Cryptography/LdSignatureTests.cs
+++ Iceshrimp.Tests/Cryptography/LdSignatureTests.cs
@@ -1,22 +1,28 @@
+using System.Diagnostics;
 using System.Security.Cryptography;
 using FluentAssertions;
 using Iceshrimp.Backend.Core.Federation.ActivityStreams;
 using Iceshrimp.Backend.Core.Federation.ActivityStreams.Types;
 using Iceshrimp.Backend.Core.Federation.Cryptography;
 using Iceshrimp.Backend.Core.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace Iceshrimp.Tests.Cryptography;
 
 [TestClass]
 public class LdSignatureTests {
-	[TestMethod]
-	public async Task RoundtripTest() {
-		var keypair = RSA.Create();
+	private ASActor _actor    = null!;
+	private RSA     _keypair  = null!;
+	private JArray  _expanded = null!;
+	private JObject _signed   = null!;
 
-		var actor = new ASActor {
+	[TestInitialize]
+	public async Task Initialize() {
+		_keypair = RSA.Create();
+		_actor = new ASActor {
 			Id             = $"https://example.org/users/{IdHelpers.GenerateSlowflakeId()}",
 			Type           = ["https://www.w3.org/ns/activitystreams#Person"],
-			Url            = new ASLink($"https://example.org/@test"),
+			Url            = new ASLink("https://example.org/@test"),
 			Username       = "test",
 			DisplayName    = "Test account",
 			IsCat          = false,
@@ -24,11 +30,71 @@ public class LdSignatureTests {
 			IsLocked       = true
 		};
 
-		var expanded  = LdHelpers.Expand(actor);
+		_expanded = LdHelpers.Expand(_actor)!;
+		_signed   = await LdSignature.Sign(_expanded, _keypair.ExportRSAPrivateKeyPem(), _actor.Id + "#main-key");
 
-		var signed = await LdSignature.Sign(expanded!, keypair.ExportRSAPrivateKeyPem(), actor.Id + "#main-key");
-		var verify = await LdSignature.Verify(signed, keypair.ExportRSAPublicKeyPem());
+		_expanded.Should().NotBeNull();
+		_signed.Should().NotBeNull();
+	}
 
+	[TestMethod]
+	public async Task RoundtripTest() {
+		var verify = await LdSignature.Verify(_signed, _keypair.ExportRSAPublicKeyPem());
 		verify.Should().BeTrue();
+	}
+
+	[TestMethod]
+	public async Task InvalidKeyTest() {
+		var rsa    = RSA.Create();
+		var verify = await LdSignature.Verify(_signed, rsa.ExportRSAPublicKeyPem());
+		verify.Should().BeFalse();
+	}
+
+	[TestMethod]
+	public async Task InvalidDataTest() {
+		var data = (_signed.DeepClone() as JObject)!;
+		data.Should().NotBeNull();
+
+		data!.Add("https://example.org/ns#test", JToken.FromObject("value"));
+		var expanded = LdHelpers.Expand(data)!;
+		expanded.Should().NotBeNull();
+
+		var verify = await LdSignature.Verify(expanded, _keypair.ExportRSAPublicKeyPem());
+		verify.Should().BeFalse();
+	}
+
+	[TestMethod]
+	public async Task MissingSignatureTest() {
+		var data = (_signed.DeepClone() as JObject)!;
+		data.Should().NotBeNull();
+
+		data.Remove("https://w3id.org/security#signature");
+		var verify = await LdSignature.Verify(data, _keypair.ExportRSAPublicKeyPem());
+		verify.Should().BeFalse();
+	}
+	
+	[TestMethod]
+	public async Task InvalidSignatureTest() {
+		var data = (_signed.DeepClone() as JObject)!;
+		data.Should().NotBeNull();
+
+		var signature = data["https://w3id.org/security#signature"]?[0]?["https://w3id.org/security#signatureValue"]?[0]?["@value"];
+		signature.Should().NotBeNull();
+
+		data["https://w3id.org/security#signature"]![0]!["https://w3id.org/security#signatureValue"]![0]!["@value"] += "test";
+		await Assert.ThrowsExceptionAsync<FormatException>(async () => await LdSignature.Verify(data, _keypair.ExportRSAPublicKeyPem()));
+	}
+	
+	[TestMethod]
+	public async Task InvalidSignatureOptionsTest() {
+		var data = (_signed.DeepClone() as JObject)!;
+		data.Should().NotBeNull();
+
+		var creator = data["https://w3id.org/security#signature"]?[0]?["http://purl.org/dc/terms/creator"]?[0]?["@value"];
+		creator.Should().NotBeNull();
+
+		data["https://w3id.org/security#signature"]![0]!["http://purl.org/dc/terms/creator"]![0]!["@value"] += "test";
+		var verify = await LdSignature.Verify(data, _keypair.ExportRSAPublicKeyPem());
+		verify.Should().BeFalse();
 	}
 }
