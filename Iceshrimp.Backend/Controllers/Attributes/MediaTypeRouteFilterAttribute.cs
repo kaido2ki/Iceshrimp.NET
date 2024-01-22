65f009ff74c575f169c635202309de9865d13933
Laura Hausmann <laura@hausmann.dev>
2024-01-22 21:35:55 +0100

    Fix MediaTypeRouteFilterAttribute

--- Iceshrimp.Backend/Controllers/Attributes/MediaTypeRouteFilterAttribute.cs
+++ Iceshrimp.Backend/Controllers/Attributes/MediaTypeRouteFilterAttribute.cs
@@ -8,7 +8,8 @@ public class MediaTypeRouteFilterAttribute(params string[] mediaTypes) : Attribu
 		//TODO: this should parse the header properly, edge cases like profile=, charset=, q= are not currently handled.
 		return context.RouteContext.HttpContext.Request.Headers.ContainsKey("Accept") &&
 		       mediaTypes.Any(p => context.RouteContext.HttpContext.Request.Headers.Accept.ToString() == p ||
-		                           p.StartsWith(context.RouteContext.HttpContext.Request.Headers.Accept + ";"));
+		                           context.RouteContext.HttpContext.Request.Headers.Accept.ToString()
+		                                  .StartsWith(p + ";"));
 	}
 
 	public int Order => HttpMethodActionConstraint.HttpMethodConstraintOrder + 1;
