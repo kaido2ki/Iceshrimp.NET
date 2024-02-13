8c015815bc369747c6dd27892d1f6c6ae340d2dc
Laura Hausmann <laura@hausmann.dev>
2024-02-13 03:01:39 +0100

    [backend/database] Remove length restriction from DriveFile comment column

--- Iceshrimp.Backend/Core/Database/Migrations/20240213015921_RemoveDriveFileCommentLengthRestriction.Designer.cs
+++ Iceshrimp.Backend/Core/Database/Migrations/20240213015921_RemoveDriveFileCommentLengthRestriction.Designer.cs
@@ -6,6 +6,7 @@ using Iceshrimp.Backend.Core.Database;
 using Iceshrimp.Backend.Core.Database.Tables;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -14,9 +15,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Iceshrimp.Backend.Core.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240213015921_RemoveDriveFileCommentLengthRestriction")]
+    partial class RemoveDriveFileCommentLengthRestriction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Iceshrimp.Backend/Core/Database/Migrations/20240213015921_RemoveDriveFileCommentLengthRestriction.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iceshrimp.Backend.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDriveFileCommentLengthRestriction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "comment",
+                table: "drive_file",
+                type: "text",
+                nullable: true,
+                comment: "The comment of the DriveFile.",
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true,
+                oldComment: "The comment of the DriveFile.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "comment",
+                table: "drive_file",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                comment: "The comment of the DriveFile.",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldComment: "The comment of the DriveFile.");
+        }
+    }
+}
