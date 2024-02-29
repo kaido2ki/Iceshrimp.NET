0b6ac4125725b1bc9bc28d75b63e1b05ddfcc64d
Laura Hausmann <laura@hausmann.dev>
2024-02-29 04:15:37 +0100

    [backend/masto-client] Fix rendering of statuses with no text in applications that rely on a null value being present

--- Iceshrimp.Backend/Controllers/Mastodon/Schemas/Entities/StatusEntity.cs
+++ Iceshrimp.Backend/Controllers/Mastodon/Schemas/Entities/StatusEntity.cs
@@ -12,6 +12,7 @@ public class StatusEntity : IEntity
 	[J("text")] [JI(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public required string? Text;
 
+	[J("content")]                public required string?       Content        { get; set; }
 	[J("uri")]                    public required string        Uri            { get; set; }
 	[J("url")]                    public required string        Url            { get; set; }
 	[J("account")]                public required AccountEntity Account        { get; set; }
@@ -32,10 +33,6 @@ public class StatusEntity : IEntity
 	[J("sensitive")]              public required bool          IsSensitive    { get; set; }
 	[J("spoiler_text")]           public required string        ContentWarning { get; set; }
 	[J("visibility")]             public required string        Visibility     { get; set; }
-
-	[J("content")]
-	[JI(Condition = JsonIgnoreCondition.WhenWritingNull)]
-	public required string? Content { get; set; }
 
 	[J("pinned")]
 	[JI(Condition = JsonIgnoreCondition.WhenWritingNull)]
