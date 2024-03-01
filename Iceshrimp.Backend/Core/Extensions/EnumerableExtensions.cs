1a86ea990a5b968659ad70fa1d8bf77fb325ce0b
Laura Hausmann <laura@hausmann.dev>
2024-03-01 18:23:44 +0100

    [backend/core] Improve TaskExtensions

--- Iceshrimp.Backend/Core/Extensions/EnumerableExtensions.cs
+++ Iceshrimp.Backend/Core/Extensions/EnumerableExtensions.cs
@@ -7,14 +7,23 @@ public static class EnumerableExtensions
 		return await Task.WhenAll(tasks);
 	}
 
+	public static async Task AwaitAllAsync(this IEnumerable<Task> tasks)
+	{
+		await Task.WhenAll(tasks);
+	}
+
 	public static async Task<List<T>> AwaitAllNoConcurrencyAsync<T>(this IEnumerable<Task<T>> tasks)
 	{
 		var results = new List<T>();
+
 		foreach (var task in tasks)
-		{
 			results.Add(await task);
-		}
 
 		return results;
+	}
+
+	public static async Task AwaitAllNoConcurrencyAsync(this IEnumerable<Task> tasks)
+	{
+		foreach (var task in tasks) await task;
 	}
 }
