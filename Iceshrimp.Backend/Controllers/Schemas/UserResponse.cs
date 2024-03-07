992189e8241d1b04adef76c67a9d61fe9118462c
Laura Hausmann <laura@hausmann.dev>
2024-03-07 05:37:53 +0100

    [backend/api] Fix json property name in UserResponse

--- Iceshrimp.Backend/Controllers/Schemas/UserResponse.cs
+++ Iceshrimp.Backend/Controllers/Schemas/UserResponse.cs
@@ -6,7 +6,7 @@ public class UserResponse
 {
 	[J("id")]              public required string  Id              { get; set; }
 	[J("username")]        public required string  Username        { get; set; }
-	[J("username")]        public required string? DisplayName     { get; set; }
+	[J("displayName")]     public required string? DisplayName     { get; set; }
 	[J("avatarUrl")]       public required string? AvatarUrl       { get; set; }
 	[J("bannerUrl")]       public required string? BannerUrl       { get; set; }
 	[J("instanceName")]    public required string? InstanceName    { get; set; }
