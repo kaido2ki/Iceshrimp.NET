939795ea72b38d3d53d5d4a0db431a1b11936810
Laura Hausmann <laura@hausmann.dev>
2024-04-04 23:14:01 +0200

    [backend/razor] Add landing page (ISH-245)

---
+++ Iceshrimp.Backend/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Iceshrimp.Backend.Core.Extensions;
+using Iceshrimp.Backend.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Iceshrimp.Backend.Pages;
+
+public class IndexModel(MetaService meta) : PageModel
+{
+	public string  InstanceName        = null!;
+	public string  InstanceDescription = null!;
+	public string? ContactEmail;
+
+	public async Task<IActionResult> OnGet()
+	{
+		if (Request.Cookies.ContainsKey("session"))
+			return Partial("Shared/FrontendSPA");
+
+		var (instanceName, instanceDescription, contactEmail) =
+			await meta.GetMany(MetaEntity.InstanceName, MetaEntity.InstanceDescription, MetaEntity.AdminContactEmail);
+
+		InstanceName = instanceName ?? "Iceshrimp.NET";
+		InstanceDescription =
+			instanceDescription ?? "This Iceshrimp.NET instance does not appear to have a description";
+		ContactEmail = contactEmail;
+
+		return Page();
+	}
+}
