333611f65eb2a65b2779ece0435b5ba84bf60e99
Laura Hausmann <laura@hausmann.dev>
2024-04-18 20:39:11 +0200

    [backend/federation] Send remote reacts as anonymous emoji objects, set correct id for local emoji reacts

--- Iceshrimp.Backend/Core/Federation/ActivityPub/ActivityRenderer.cs
+++ Iceshrimp.Backend/Core/Federation/ActivityPub/ActivityRenderer.cs
@@ -97,7 +97,7 @@ public class ActivityRenderer(
 
 			var e = new ASEmoji
 			{
-				Id    = emoji.PublicUrl,
+				Id    = emoji.Host == null ? $"https://{config.Value.WebDomain}/emoji/{emoji.Name}" : null,
 				Name  = name,
 				Image = new ASImage { Url = new ASLink(emoji.PublicUrl) }
 			};
