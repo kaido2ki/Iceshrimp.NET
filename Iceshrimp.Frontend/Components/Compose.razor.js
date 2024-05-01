e793abab8b1445f6337503c0d5a29539cc93985e
Lilian <lilian@blahaj.space>
2024-05-01 02:46:02 +0200

    [frontend] Fix compose menu not opening

--- Iceshrimp.Frontend/Components/Compose.razor.js
+++ Iceshrimp.Frontend/Components/Compose.razor.js
@@ -1,6 +1,8 @@
 export function openDialog(element){
+    console.log("opening JS")
     element.showModal()
 }
 export function closeDialog(element){
+    console.log("closing JS")
     element.close()
 }
