ed04ab95425079105b9009cc426507ebfb7b3110
Laura Hausmann <laura@hausmann.dev>
2024-05-14 20:15:49 +0200

    [backend/database] Add migration that fixes up inconsistent drive file metadata

---
+++ Iceshrimp.Backend/Core/Database/Migrations/20240514181009_FixupDriveFileMetadata.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+#nullable disable
+
+namespace Iceshrimp.Backend.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(DatabaseContext))]
+    [Migration("20240514181009_FixupDriveFileMetadata")]
+    public partial class FixupDriveFileMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.Sql("UPDATE "drive_file" SET "thumbnailAccessKey" = NULL WHERE "thumbnailUrl" IS NULL AND "thumbnailAccessKey" IS NOT NULL;");
+	        migrationBuilder.Sql("UPDATE "drive_file" SET "webpublicAccessKey" = NULL WHERE "webpublicUrl" IS NULL AND "webpublicAccessKey" IS NOT NULL;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
