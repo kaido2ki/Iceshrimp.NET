kaido2ki/Iceshrimp.NET

commit 3d15ed68073c801188f07335cd377b3f5275a43b

[backend/asp] Add explicit routes for matching local and remote users
This is necessary because MapFallbackToPage doesn't apply to routes containing special characters (dots, @ symbol) by default.

Iceshrimp.Backend/Startup.cs
@@ -69,7 +69,7 @@ app.MapFallbackToController("/api/{**slug}", "FallbackAction", "Fallback");
 app.MapHub<ExampleHub>("/hubs/example");
 app.MapHub<StreamingHub>("/hubs/streaming");
 app.MapRazorPages();
-app.MapFallbackToPage("/Shared/FrontendSPA");
+app.MapFrontendRoutes("/Shared/FrontendSPA");
 
 app.Urls.Clear();
 if (config.ListenSocket == null)

Iceshrimp.Backend/Core/Extensions/WebApplicationExtensions.cs
@@ -46,6 +46,13 @@ public static class WebApplicationExtensions
 		return app;
 	}
 
+	public static void MapFrontendRoutes(this WebApplication app, string page)
+	{
+		app.MapFallbackToPage(page).WithOrder(int.MaxValue - 2);
+		app.MapFallbackToPage("/@{user}", page).WithOrder(int.MaxValue - 1);
+		app.MapFallbackToPage("/@{user}@{host}", page);
+	}
+
 	public static async Task<Config.InstanceSection> Initialize(this WebApplication app, string[] args)
 	{
 		var instanceConfig = app.Configuration.GetSection("Instance").Get<Config.InstanceSection>() ??
@@ -193,7 +200,7 @@ public static class WebApplicationExtensions
 
 		app.Logger.LogInformation("Warming up meta cache...");
 		await meta.WarmupCache();
-		
+
 		// Initialize image processing
 		provider.GetRequiredService<ImageProcessor>();
 

commit e1f525a4a3965ecdc4658566f309d097986618ee

[backend/asp] Fix api controller fallback action not getting executed (ISH-356)
This fixes a regression introduced in 3d15ed68073c801188f07335cd377b3f5275a43b.

Iceshrimp.Backend/Startup.cs
@@ -65,7 +65,7 @@ app.UseWebSockets(new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSecond
 app.UseCustomMiddleware();
 
 app.MapControllers();
-app.MapFallbackToController("/api/{**slug}", "FallbackAction", "Fallback");
+app.MapFallbackToController("/api/{**slug}", "FallbackAction", "Fallback").WithOrder(int.MaxValue - 3);
 app.MapHub<ExampleHub>("/hubs/example");
 app.MapHub<StreamingHub>("/hubs/streaming");
 app.MapRazorPages();