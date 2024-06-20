4c7b1c38691395991011678609bc1a5f61a0e8e8
Lilian <lilian@blahaj.space>
2024-06-21 00:41:16 +0200

    [frontend] Add admin_session cookie

--- Iceshrimp.Frontend/Core/Services/SessionService.cs
+++ Iceshrimp.Frontend/Core/Services/SessionService.cs
@@ -7,72 +7,81 @@ namespace Iceshrimp.Frontend.Core.Services;
 
 internal class SessionService
 {
-	[Inject] public ISyncLocalStorageService       LocalStorage { get; }
-	[Inject] public ApiService                     ApiService   { get; }
-	[Inject] public IJSRuntime                     Js           { get; }
-	public          Dictionary<string, StoredUser> Users        { get; }
-	public          StoredUser?                    Current      { get; private set; }
+    [Inject] public ISyncLocalStorageService       LocalStorage { get; }
+    [Inject] public ApiService                     ApiService   { get; }
+    [Inject] public IJSRuntime                     Js           { get; }
+    public          Dictionary<string, StoredUser> Users        { get; }
+    public          StoredUser?                    Current      { get; private set; }
 
-	public SessionService(ApiService apiService, ISyncLocalStorageService localStorage, IJSRuntime js)
-	{
-		ApiService   = apiService;
-		LocalStorage = localStorage;
-		Js           = js;
-		Users        = LocalStorage.GetItem<Dictionary<string, StoredUser>>("Users") ?? [];
-		var lastUser = LocalStorage.GetItem<string?>("last_user");
-		if (lastUser != null)
-		{
-			SetSession(lastUser);
-		}
-	}
+    public SessionService(ApiService apiService, ISyncLocalStorageService localStorage, IJSRuntime js)
+    {
+        ApiService   = apiService;
+        LocalStorage = localStorage;
+        Js           = js;
+        Users        = LocalStorage.GetItem<Dictionary<string, StoredUser>>("Users") ?? [];
+        var lastUser = LocalStorage.GetItem<string?>("last_user");
+        if (lastUser != null)
+        {
+            SetSession(lastUser);
+        }
+    }
 
-	private void WriteUsers()
-	{
-		LocalStorage.SetItem("Users", Users);
-	}
+    private void WriteUsers()
+    {
+        LocalStorage.SetItem("Users", Users);
+    }
 
-	public void AddUser(StoredUser user)
-	{
-		try
-		{
-			Users.Add(user.Id, user);
-		}
-		catch (ArgumentException) // Update user if it already exists.
-		{
-			Users[user.Id] = user;
-		}
+    public void AddUser(StoredUser user)
+    {
+        try
+        {
+            Users.Add(user.Id, user);
+        }
+        catch (ArgumentException) // Update user if it already exists.
+        {
+            Users[user.Id] = user;
+        }
 
-		WriteUsers();
-	}
+        WriteUsers();
+    }
 
-	public void DeleteUser(string id)
-	{
-		if (id == Current?.Id) throw new ArgumentException("Cannot remove current user.");
-		Users.Remove(id);
-		WriteUsers();
-	}
+    public void DeleteUser(string id)
+    {
+        if (id == Current?.Id) throw new ArgumentException("Cannot remove current user.");
+        Users.Remove(id);
+        WriteUsers();
+    }
 
-	private StoredUser GetUserById(string id)
-	{
-		var user = Users[id];
-		return user;
-	}
+    private StoredUser GetUserById(string id)
+    {
+        var user = Users[id];
+        return user;
+    }
 
-	public void EndSession()
-	{
-		Current = null;
-		LocalStorage.RemoveItem("last_user");
-	}
+    public void EndSession()
+    {
+        Current = null;
+        LocalStorage.RemoveItem("last_user");
+        ((IJSInProcessRuntime)Js).InvokeVoid("eval",
+                                             $"document.cookie = \"admin_session=; Fri, 31 Dec 1000 23:59:59 GMT SameSite=Strict\"");
+    }
 
-	public void SetSession(string id)
-	{
-		var user = GetUserById(id);
-		if (user == null) throw new Exception("Did not find User in Local Storage");
-		ApiService.SetBearerToken(user.Token);
-		Current = user;
-		LocalStorage.SetItem("last_user", user.Id);
-		((IJSInProcessRuntime)Js).InvokeVoid("eval",
-		$"document.cookie = \"session={user.Id}; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=Strict\"");
-		// Security implications of this need a second pass? user.Id should never be user controllable, but still.
-	}
+    public void SetSession(string id)
+    {
+        ((IJSInProcessRuntime)Js).InvokeVoid("eval",
+                                             $"document.cookie = \"admin_session=; expires=Fri, 31 Dec 1000 23:59:59 GMT SameSite=Strict\"");
+        var user = GetUserById(id);
+        if (user == null) throw new Exception("Did not find User in Local Storage");
+        ApiService.SetBearerToken(user.Token);
+        Current = user;
+        LocalStorage.SetItem("last_user", user.Id);
+        ((IJSInProcessRuntime)Js).InvokeVoid("eval",
+                                             $"document.cookie = \"session={user.Id}; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=Strict\"");
+        if (user.IsAdmin)
+        {
+            ((IJSInProcessRuntime)Js).InvokeVoid("eval",
+                                                 $"document.cookie = \"admin_session={user.Token}; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=Strict\"");
+        }
+        // Security implications of this need a second pass? user.Id should never be user controllable, but still.
+    }
 }
