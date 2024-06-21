c49973becfdd4594eaacea8520dc0a6c36f758de
Laura Hausmann <laura@hausmann.dev>
2024-06-21 16:24:08 +0200

    [backend] Code cleanup

--- Iceshrimp.Backend/Pages/QueueJob.cshtml.cs
+++ Iceshrimp.Backend/Pages/QueueJob.cshtml.cs
@@ -1,7 +1,6 @@
 using Iceshrimp.Backend.Core.Database;
 using Iceshrimp.Backend.Core.Database.Tables;
 using Iceshrimp.Backend.Core.Middleware;
-using Iceshrimp.Backend.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
