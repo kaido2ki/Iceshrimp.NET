8a80c69b916cac27c6039253d4f77fb7ec0756e0
pancakes <trypancakes@tutanota.com>
2024-06-22 13:31:28 +1000

    [backend/api] Create all required fields and return EmojiResponse

---
+++ Iceshrimp.Shared/Schemas/EmojiResponse.cs
@@ -0,0 +1,12 @@
+namespace Iceshrimp.Shared.Schemas;
+
+public class EmojiResponse
+{
+    public required string       Id        { get; set; }
+    public required string       Name      { get; set; }
+    public required string?      Uri       { get; set; }
+    public required List<string> Aliases   { get; set; }
+    public required string?      Category  { get; set; }
+    public required string       PublicUrl { get; set; }
+    public required string?      License   { get; set; }
+}
