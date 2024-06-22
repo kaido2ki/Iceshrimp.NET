8d432a5e2628ad19a1e34e9e0e9d26fc6d8865ae
pancakes <trypancakes@tutanota.com>
2024-06-22 13:35:27 +1000

    [backend/api] Add PATCH admin/emoji/{id} and UpdateEmojiRequest

---
+++ Iceshrimp.Shared/Schemas/UpdateEmojiRequest.cs
@@ -0,0 +1,9 @@
+namespace Iceshrimp.Shared.Schemas;
+
+public class UpdateEmojiRequest
+{
+    public string?       Name     { get; set; }
+    public List<string>? Aliases  { get; set; }
+    public string?       Category { get; set; }
+    public string?       License  { get; set; }
+}
