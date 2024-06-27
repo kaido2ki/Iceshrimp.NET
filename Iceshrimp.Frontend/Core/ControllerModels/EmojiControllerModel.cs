c9b282a90e14bafff2b6e3d1fc29d6cf06eaba72
Lilian <lilian@blahaj.space>
2024-06-28 00:44:26 +0200

    [frontend/api] Add EmojiControllerModel

---
+++ Iceshrimp.Frontend/Core/ControllerModels/EmojiControllerModel.cs
@@ -0,0 +1,20 @@
+using Iceshrimp.Frontend.Core.Services;
+using Iceshrimp.Shared.Schemas;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Iceshrimp.Frontend.Core.ControllerModels;
+
+internal class EmojiControllerModel(ApiClient api)
+{
+    public Task<List<EmojiResponse>> GetAllEmoji() =>
+        api.Call<List<EmojiResponse>>(HttpMethod.Get, "/emoji");
+
+    public Task<EmojiResponse> UploadEmoji(IBrowserFile file) =>
+        api.Call<EmojiResponse>(HttpMethod.Post, "/emoji", data: file);
+
+    public Task<EmojiResponse?> UpdateEmoji(string id, EmojiResponse emoji) =>
+        api.CallNullable<EmojiResponse>(HttpMethod.Patch, $"/emoji/{id}", data: emoji);
+
+    public Task<EmojiResponse?> GetEmoji(string id) =>
+        api.CallNullable<EmojiResponse>(HttpMethod.Get, $"/emoji/{id}");
+}
