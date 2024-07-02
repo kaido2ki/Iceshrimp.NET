f2eec7877c9b9fd5ebdc1372dc38e2c2e6193fcf
Laura Hausmann <laura@hausmann.dev>
2024-07-03 01:49:04 +0200

    [docker] Use buildkit for building docker images

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-wasm --push
-docker build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-alpine.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-alpine --push
-docker build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-9.0-alpine-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:9.0-alpine-wasm --push
-docker build . --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet --push
-docker build . --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet-wasm --push
+set -e
+docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-wasm --push
+docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-alpine.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-alpine --push
+docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-9.0-alpine-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:9.0-alpine-wasm --push
+docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet --push
+docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet-wasm --push
