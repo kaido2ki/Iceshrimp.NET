47491bdde1a48ee79bbbcf861f2fc68ed55bd476
Laura Hausmann <laura@hausmann.dev>
2024-07-06 18:48:52 +0200

    [docker] Build images with buildx cluster

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
-docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-alpine.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-alpine --push
-docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-wasm --push
-#docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-9.0-alpine-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:9.0-alpine-wasm --push
-docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet --push
-docker buildx build . --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet-wasm --push
+docker buildx build . --builder build-cluster --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-alpine.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-alpine --push
+docker buildx build . --builder build-cluster --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-8.0-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:8.0-wasm --push
+
+docker buildx build . --builder build-cluster --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-9.0-alpine.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:9.0-alpine --push
+docker buildx build . --builder build-cluster --platform=linux/amd64,linux/arm64 --provenance=false -f dotnet-sdk-9.0-alpine-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/dotnet-sdk:9.0-alpine-wasm --push
+
+docker buildx build . --builder build-cluster --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet --push
+docker buildx build . --builder build-cluster --platform=linux/amd64,linux/arm64 --provenance=false -f ci-env-wasm.Dockerfile -t iceshrimp.dev/iceshrimp/ci-env:dotnet-wasm --push
+
+docker buildx prune -a --builder build-cluster
