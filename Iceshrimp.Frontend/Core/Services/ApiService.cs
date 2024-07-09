d36a9ec2356f55bce98b9fc5c5fd941c0253cc41
Lilian <lilian@blahaj.space>
2024-07-09 02:36:43 +0200

    [frontend] Actually load FollowRequestControllerModel

--- Iceshrimp.Frontend/Core/Services/ApiService.cs
+++ Iceshrimp.Frontend/Core/Services/ApiService.cs
@@ -4,16 +4,17 @@ namespace Iceshrimp.Frontend.Core.Services;
 
 internal class ApiService(ApiClient client)
 {
-	public readonly AdminControllerModel        Admin         = new(client);
-	public readonly AuthControllerModel         Auth          = new(client);
-	public readonly DriveControllerModel        Drive         = new(client);
-	public readonly EmojiControllerModel        Emoji         = new(client);
-	public readonly NoteControllerModel         Notes         = new(client);
-	public readonly NotificationControllerModel Notifications = new(client);
-	public readonly SearchControllerModel       Search        = new(client);
-	public readonly SettingsControllerModel     Settings      = new(client);
-	public readonly TimelineControllerModel     Timelines     = new(client);
-	public readonly UserControllerModel         Users         = new(client);
+	public readonly AdminControllerModel         Admin          = new(client);
+	public readonly AuthControllerModel          Auth           = new(client);
+	public readonly DriveControllerModel         Drive          = new(client);
+	public readonly EmojiControllerModel         Emoji          = new(client);
+	public readonly NoteControllerModel          Notes          = new(client);
+	public readonly NotificationControllerModel  Notifications  = new(client);
+	public readonly SearchControllerModel        Search         = new(client);
+	public readonly SettingsControllerModel      Settings       = new(client);
+	public readonly TimelineControllerModel      Timelines      = new(client);
+	public readonly UserControllerModel          Users          = new(client);
+	public readonly FollowRequestControllerModel FollowRequests = new(client);
 
 	public void SetBearerToken(string token) => client.SetToken(token);
 }
