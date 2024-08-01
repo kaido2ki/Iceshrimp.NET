e298b35c61b6bd47dcdfccff776d1b96c7bd9b63
Laura Hausmann <laura@hausmann.dev>
2024-08-01 20:04:28 +0200

    [backend/tests] Switch to ConfigurationBuilder.AddIniStream

--- Iceshrimp.Tests/MockObjects.cs
+++ Iceshrimp.Tests/MockObjects.cs
@@ -39,7 +39,7 @@ public static class MockObjects
 	private static ServiceProvider GetServiceProvider()
 	{
 		var config = new ConfigurationManager();
-		config.AddIniFile("configuration.ini", false);
+		config.AddIniStream(AssemblyHelpers.GetEmbeddedResourceStream("configuration.ini"));
 
 		var collection = new ServiceCollection();
 		collection.AddServices();
