e342d6f010445526c22456704f756b6fa4a08575
Laura Hausmann <laura@hausmann.dev>
2024-08-16 01:34:43 +0200

    [backend/asp] Improve XML error responses

--- Iceshrimp.Shared/Schemas/Web/ErrorResponse.cs
+++ Iceshrimp.Shared/Schemas/Web/ErrorResponse.cs
@@ -7,8 +7,8 @@ namespace Iceshrimp.Shared.Schemas.Web;
 [XmlRoot("Error")]
 public class ErrorResponse
 {
-	[XmlElement("Code")]  public required int    StatusCode { get; set; }
-	[XmlElement("Error")] public required string Error      { get; set; }
+	[XmlElement("Status")] public required int    StatusCode { get; set; }
+	[XmlElement("Error")]  public required string Error      { get; set; }
 
 	[XmlElement("Message")]
 	[JI(Condition = JsonIgnoreCondition.WhenWritingNull)]
@@ -22,10 +22,25 @@ public class ErrorResponse
 	[JI(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public IDictionary<string, string[]>? Errors { get; set; }
 
+	[XmlArray("ValidationErrors")]
+	[XmlArrayItem("Error")]
+	[JI]
+	public XmlValidationError[]? XmlErrors
+	{
+		get => Errors?.SelectMany(x => x.Value.Select(i => new XmlValidationError { Element = x.Key, Error = i }))
+		             .ToArray();
+		set => Errors = value?.ToDictionary(p => p.Element, p => (string[]) [p.Error]);
+	}
+
 	[XmlElement("Source")]
 	[JI(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? Source { get; set; }
 
-	[XmlElement("RequestId")]
-	public required string RequestId { get; set; }
+	[XmlElement("RequestId")] public required string RequestId { get; set; }
+}
+
+public class XmlValidationError
+{
+	[XmlAttribute("Element")] public required string Element { get; set; }
+	[XmlAttribute("Error")]   public required string Error   { get; set; }
 }
