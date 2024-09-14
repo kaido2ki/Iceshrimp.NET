238b4415606c75bf5a07d12d90612cb2d8f66527
Laura Hausmann <laura@hausmann.dev>
2024-09-14 02:46:31 +0200

    [backend/database] Remove remote user reply notifications

---
+++ Iceshrimp.Backend/Core/Database/Migrations/v2024.1-beta4/20240914004319_RemoveRemoteReplyNotifications.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+#nullable disable
+
+namespace Iceshrimp.Backend.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(DatabaseContext))]
+    [Migration("20240914004319_RemoveRemoteReplyNotifications")]
+    public partial class RemoveRemoteReplyNotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.Sql("""
+	                             DELETE FROM "notification" WHERE "notifieeId" NOT IN (SELECT "id" FROM "user" WHERE "host" IS NULL);
+	                             """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
