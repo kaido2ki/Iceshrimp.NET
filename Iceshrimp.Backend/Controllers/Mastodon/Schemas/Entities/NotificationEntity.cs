13955e7efe76a7ec8b7317395e8d03517678e15f
pancakes <trypancakes@tutanota.com>
2024-10-22 15:01:35 +1000

    [backend/masto-client] Move Bite from Pleroma to Mastodon

--- Iceshrimp.Backend/Controllers/Mastodon/Schemas/Entities/NotificationEntity.cs
+++ Iceshrimp.Backend/Controllers/Mastodon/Schemas/Entities/NotificationEntity.cs
@@ -36,7 +36,7 @@ public class NotificationEntity : IEntity
 			NotificationType.Reaction when isPleroma  => "pleroma:emoji_reaction",
 			NotificationType.Reaction when !isPleroma => "reaction",
 			
-			NotificationType.Bite when isPleroma => "bite",
+			NotificationType.Bite => "bite",
 
 			_ => throw new GracefulException($"Unsupported notification type: {type}")
 		};
