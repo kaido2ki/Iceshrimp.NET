346803935ac0245825a22822c1ed8c526974b126
Laura Hausmann <laura@hausmann.dev>
2024-11-06 03:41:23 +0100

    [backend/razor] Add a navigation bar to the admin dashboard
    This is implemented using a reusable navigation bar component.

---
+++ Iceshrimp.Backend/Components/Generic/NavBar.razor.js
@@ -0,0 +1,11 @@
+function toggleHamburger(e) {
+    e.preventDefault()
+
+    for (let el of document.getElementsByClassName("hamburger-menu")) {
+        if (el.classList.contains("hidden")) {
+            el.classList.remove('hidden');
+        } else {
+            el.classList.add('hidden');
+        }
+    }
+}
