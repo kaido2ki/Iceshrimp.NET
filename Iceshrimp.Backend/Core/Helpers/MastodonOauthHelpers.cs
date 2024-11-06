11170ffbccb0818504f078ff711eee518fa998c1
Kopper <kopper@w.on-t.work>
2024-11-06 13:31:51 +0300

    [backend/masto-client] Allow admin:read and admin:write scopes
    These aren't actually used anywhere but supported just to make sure
    clients that specify them in their scopes can log in successfully.

--- Iceshrimp.Backend/Core/Helpers/MastodonOauthHelpers.cs
+++ Iceshrimp.Backend/Core/Helpers/MastodonOauthHelpers.cs
@@ -17,6 +17,17 @@ public static class MastodonOauthHelpers
 		"read:statuses"
 	];
 
+	private static readonly List<string> AdminReadScopes =
+	[
+		"admin:read:accounts",
+		"admin:read:reports",
+		"admin:read:domain_allows",
+		"admin:read:domain_blocks",
+		"admin:read:ip_blocks",
+		"admin:read:email_domain_blocks",
+		"admin:read:canonical_email_blocks"
+	];
+
 	private static readonly List<string> WriteScopes =
 	[
 		"write:accounts",
@@ -35,11 +46,23 @@ public static class MastodonOauthHelpers
 		"write:bites"
 	];
 
+	private static readonly List<string> AdminWriteScopes =
+	[
+		"admin:write:accounts",
+		"admin:write:reports",
+		"admin:write:domain_allows",
+		"admin:write:domain_blocks",
+		"admin:write:ip_blocks",
+		"admin:write:email_domain_blocks",
+		"admin:write:canonical_email_blocks"
+	];
+
 	private static readonly List<string> FollowScopes =
 	[
 		"read:follows", "read:blocks", "read:mutes", "write:follows", "write:blocks", "write:mutes"
 	];
 
+	private static readonly List<string> AdminScopeGroups = ["admin:read", "admin:write"];
 	private static readonly List<string> ScopeGroups = ["read", "write", "follow", "push", "admin"];
 
 	private static readonly List<string> ForbiddenSchemes = ["javascript", "file", "data", "mailto", "tel"];
@@ -54,6 +77,12 @@ public static class MastodonOauthHelpers
 				res.AddRange(WriteScopes);
 			else if (scope == "follow")
 				res.AddRange(FollowScopes);
+			else if (scope == "admin")
+				res.AddRange(ExpandScopes(AdminScopeGroups));
+			else if (scope == "admin:read")
+				res.AddRange(AdminReadScopes);
+			else if (scope == "admin:write")
+				res.AddRange(AdminWriteScopes);
 			else
 				res.Add(scope);
 
