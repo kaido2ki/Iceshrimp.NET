ee9279bb5fd17a67ba1a2eb6c53a6870aee362cb
Laura Hausmann <laura@hausmann.dev>
2024-11-19 03:14:13 +0100

    [build] Disable gzip compression during build

--- Iceshrimp.Build/RewriteStaticAssetManifestTask.cs
+++ Iceshrimp.Build/RewriteStaticAssetManifestTask.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.Build.Framework;
 
@@ -8,19 +7,14 @@ namespace Iceshrimp.Build.Tasks;
 
 public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 {
-	public static void FixupFile(string manifestPath)
-	{
-		var parsed = Parse(manifestPath);
-		var @fixed = Fixup(manifestPath, parsed);
-		Write(manifestPath, @fixed);
-	}
-
 	[System.ComponentModel.DataAnnotations.Required]
-	public required ITaskItem[] ManifestFiles { get; set; }
+	public required ITaskItem Manifest { get; set; }
 
 	public override bool Execute()
 	{
-		foreach (var item in ManifestFiles) FixupFile(item.ItemSpec);
+		var parsed = Parse(Manifest.ItemSpec);
+		var @fixed = Fixup(Manifest.ItemSpec, parsed);
+		Write(Manifest.ItemSpec, @fixed);
 		return true;
 	}
 
@@ -54,15 +48,6 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 		                                         p => p.ResponseHeaders
 		                                               .FirstOrDefault(i => i.Name == "Content-Length"));
 
-		// Remove gzip-compressed versions
-		foreach (var endpoint in manifest.Endpoints.ToArray())
-		{
-			if (!endpoint.Selectors.Any(p => p is { Name: "Content-Encoding", Value: "gzip" }))
-				continue;
-
-			manifest.Endpoints.Remove(endpoint);
-		}
-
 		// Rewrite uncompressed versions to reference brotli-compressed asset instead
 		foreach (var endpoint in manifest.Endpoints.ToArray())
 		{
@@ -75,7 +60,7 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 			endpoint.ResponseHeaders.Add(len);
 			endpoint.AssetPath += ".br";
 		}
-		
+
 		// Remove explicit routes
 		manifest.Endpoints.RemoveAll(p => p.Route.EndsWith(".br"));
 
@@ -93,15 +78,9 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 
 		// ReSharper disable once CollectionNeverUpdated.Local
 		public List<StaticAssetDescriptor> Endpoints { get; set; } = [];
-
-		public bool IsBuildManifest() => string.Equals(ManifestType, "Build", StringComparison.OrdinalIgnoreCase);
 	}
 
-	/// <summary>
-	/// The description of a static asset that was generated during the build process.
-	/// </summary>
-	[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-	public sealed class StaticAssetDescriptor
+	private sealed class StaticAssetDescriptor
 	{
 		private string?                         _route;
 		private string?                         _assetFile;
@@ -109,18 +88,12 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 		private List<StaticAssetProperty>       _endpointProperties = [];
 		private List<StaticAssetResponseHeader> _responseHeaders    = [];
 
-		/// <summary>
-		/// The route that the asset is served from.
-		/// </summary>
 		public required string Route
 		{
 			get => _route ?? throw new InvalidOperationException("Route is required");
 			set => _route = value;
 		}
 
-		/// <summary>
-		/// The path to the asset file from the wwwroot folder.
-		/// </summary>
 		[JsonPropertyName("AssetFile")]
 		public required string AssetPath
 		{
@@ -128,9 +101,6 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 			set => _assetFile = value;
 		}
 
-		/// <summary>
-		/// A list of selectors that are used to discriminate between two or more assets with the same route.
-		/// </summary>
 		[JsonPropertyName("Selectors")]
 		public List<StaticAssetSelector> Selectors
 		{
@@ -138,9 +108,6 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 			set => _selectors = value;
 		}
 
-		/// <summary>
-		/// A list of properties that are associated with the endpoint.
-		/// </summary>
 		[JsonPropertyName("EndpointProperties")]
 		public List<StaticAssetProperty> Properties
 		{
@@ -148,87 +115,29 @@ public class RewriteStaticAssetManifest : Microsoft.Build.Utilities.Task
 			set => _endpointProperties = value;
 		}
 
-		/// <summary>
-		/// A list of headers to apply to the response when this resource is served.
-		/// </summary>
 		[JsonPropertyName("ResponseHeaders")]
 		public List<StaticAssetResponseHeader> ResponseHeaders
 		{
 			get => _responseHeaders;
 			set => _responseHeaders = value;
 		}
-
-		private string GetDebuggerDisplay()
-		{
-			return $"Route: {Route} Path: {AssetPath}";
-		}
 	}
 
-	/// <summary>
-	/// A static asset selector. Selectors are used to discriminate between two or more assets with the same route.
-	/// </summary>
-	/// <param name="name">The name associated to the selector.</param>
-	/// <param name="value">The value associated to the selector and used to match against incoming requests.</param>
-	/// <param name="quality">The static server quality associated to this selector.</param>
-	[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-	public sealed class StaticAssetSelector(string name, string value, string quality)
+	private sealed class StaticAssetSelector(string name, string value)
 	{
-		/// <summary>
-		/// The name associated to the selector.
-		/// </summary>
 		public string Name { get; } = name;
-
-		/// <summary>
-		/// The value associated to the selector and used to match against incoming requests.
-		/// </summary>
 		public string Value { get; } = value;
-
-		/// <summary>
-		/// The static asset server quality associated to this selector. Used to break ties when a request matches multiple values
-		/// with the same degree of specificity.
-		/// </summary>
-		public string Quality { get; } = quality;
-
-		private string GetDebuggerDisplay() => $"Name: {Name} Value: {Value} Quality: {Quality}";
 	}
 
-	/// <summary>
-	/// A property associated with a static asset.
-	/// </summary>
-	[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-	public sealed class StaticAssetProperty(string name, string value)
+	private sealed class StaticAssetProperty(string name, string value)
 	{
-		/// <summary>
-		/// The name of the property.
-		/// </summary>
 		public string Name { get; } = name;
-
-		/// <summary>
-		/// The value of the property.
-		/// </summary>
 		public string Value { get; } = value;
-
-		private string GetDebuggerDisplay() => $"Name: {Name} Value:{Value}";
 	}
 
-	/// <summary>
-	/// A response header to apply to the response when a static asset is served.
-	/// </summary>
-	/// <param name="name">The name of the header.</param>
-	/// <param name="value">The value of the header.</param>
-	[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-	public sealed class StaticAssetResponseHeader(string name, string value)
+	private sealed class StaticAssetResponseHeader(string name, string value)
 	{
-		/// <summary>
-		/// The name of the header.
-		/// </summary>
 		public string Name { get; } = name;
-
-		/// <summary>
-		/// The value of the header.
-		/// </summary>
 		public string Value { get; } = value;
-
-		private string GetDebuggerDisplay() => $"Name: {Name} Value: {Value}";
 	}
 }
