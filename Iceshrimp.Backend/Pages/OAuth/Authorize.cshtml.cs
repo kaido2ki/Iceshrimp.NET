5cf951e908d30214a62152564416a3f0482c651a
Laura Hausmann <laura@hausmann.dev>
2024-11-21 22:04:13 +0100

    [backend/razor] Add TOTP 2FA support to OAuth page

--- Iceshrimp.Backend/Pages/OAuth/Authorize.cshtml.cs
+++ Iceshrimp.Backend/Pages/OAuth/Authorize.cshtml.cs
@@ -3,6 +3,7 @@ using Iceshrimp.Backend.Core.Database;
 using Iceshrimp.Backend.Core.Database.Tables;
 using Iceshrimp.Backend.Core.Helpers;
 using Iceshrimp.Backend.Core.Middleware;
+using Iceshrimp.Shared.Schemas.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +31,8 @@ public class AuthorizeModel(DatabaseContext db) : PageModel
 		[MemberNotNull(nameof(Scopes))] set => Scopes = value.Split(' ').ToList();
 	}
 
+	public LoginData? TwoFactorFormData = null;
+
 	public async Task OnGet()
 	{
 		if (ResponseType == null || ClientId == null || RedirectUri == null)
@@ -53,7 +56,7 @@ public class AuthorizeModel(DatabaseContext db) : PageModel
 	}
 
 	public async Task OnPost(
-		[FromForm] string? username, [FromForm] string? password, [FromForm] string? userId,
+		[FromForm] string? username, [FromForm] string? password, [FromForm] string? totp, [FromForm] string? userId,
 		[FromForm] bool supportsHtmlFormatting, [FromForm] bool autoDetectQuotes, [FromForm] bool isPleroma
 	)
 	{
@@ -77,6 +80,25 @@ public class AuthorizeModel(DatabaseContext db) : PageModel
 				throw Forbidden();
 			if (AuthHelpers.ComparePassword(password, userSettings.Password) == false)
 				throw Forbidden();
+
+			if (userSettings.TwoFactorEnabled)
+			{
+				if (!string.IsNullOrWhiteSpace(totp))
+				{
+					if (userSettings.TwoFactorSecret == null)
+						throw new Exception("2FA is enabled but secret is null");
+					if (!TotpHelper.Validate(userSettings.TwoFactorSecret, totp))
+					{
+						SetTwoFactorFormData();
+						return;
+					}
+				}
+				else
+				{
+					SetTwoFactorFormData();
+					return;
+				}
+			}
 		}
 
 		var token = new OauthToken
@@ -99,5 +121,30 @@ public class AuthorizeModel(DatabaseContext db) : PageModel
 		await db.SaveChangesAsync();
 
 		Token = token;
+		return;
+
+		void SetTwoFactorFormData()
+		{
+			TwoFactorFormData = new LoginData
+			{
+				Username               = username,
+				Password               = password,
+				AutoDetectQuotes       = autoDetectQuotes,
+				SupportsHtmlFormatting = supportsHtmlFormatting,
+				IsPleroma              = isPleroma
+			};
+
+			Response.Headers.CacheControl = "private, no-store, no-cache";
+			Response.Headers.Pragma       = "no-cache";
+		}
+	}
+
+	public class LoginData
+	{
+		public required string Username;
+		public required string Password;
+		public required bool   SupportsHtmlFormatting;
+		public required bool   AutoDetectQuotes;
+		public required bool   IsPleroma;
 	}
 }
