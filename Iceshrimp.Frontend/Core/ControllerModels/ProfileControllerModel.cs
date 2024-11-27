93fd573ca6140670fcfd76698184c097daf5860f
pancakes <trypancakes@tutanota.com>
2024-11-28 00:03:23 +1000

    [frontend/api] Make GetDisplayNameAsync not nullable

--- Iceshrimp.Frontend/Core/ControllerModels/ProfileControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/ProfileControllerModel.cs
@@ -11,8 +11,8 @@ internal class ProfileControllerModel(ApiClient api)
 	public Task UpdateProfileAsync(UserProfileEntity request) =>
 		api.CallAsync(HttpMethod.Put, "/profile", data: request);
 	
-	public Task<string?> GetDisplayNameAsync() =>
-		api.CallNullableAsync<string>(HttpMethod.Get, "/profile/display_name");
+	public Task<string> GetDisplayNameAsync() =>
+		api.CallAsync<string>(HttpMethod.Get, "/profile/display_name");
 
 	public Task<string?> UpdateDisplayNameAsync(string displayName) =>
 		api.CallNullableAsync<string>(HttpMethod.Post, "/profile/display_name", data: displayName);
