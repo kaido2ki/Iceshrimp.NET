beef9f75036c7762102c78620b04983b81e07fde
pancakes <trypancakes@tutanota.com>
2024-12-18 23:36:45 +1000

    [frontend/pages] Use DriveFileResponse for avatar and banner

--- Iceshrimp.Frontend/Core/ControllerModels/ProfileControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/ProfileControllerModel.cs
@@ -12,8 +12,8 @@ internal class ProfileControllerModel(ApiClient api)
 	public Task UpdateProfileAsync(UserProfileEntity request) =>
 		api.CallAsync(HttpMethod.Put, "/profile", data: request);
 
-	public Task<string> GetAvatarUrlAsync() =>
-		api.CallAsync<string>(HttpMethod.Get, "/profile/avatar");
+	public Task<DriveFileResponse> GetAvatarAsync() =>
+		api.CallAsync<DriveFileResponse>(HttpMethod.Get, "/profile/avatar");
 
 	public Task UpdateAvatarAsync(IBrowserFile file) =>
 		api.CallAsync(HttpMethod.Post, "/profile/avatar", data: file);
@@ -21,8 +21,8 @@ internal class ProfileControllerModel(ApiClient api)
 	public Task DeleteAvatarAsync() =>
 		api.CallAsync(HttpMethod.Delete, "/profile/avatar");
 
-	public Task<string> GetBannerUrlAsync() =>
-		api.CallAsync<string>(HttpMethod.Get, "/profile/banner");
+	public Task<DriveFileResponse> GetBannerAsync() =>
+		api.CallAsync<DriveFileResponse>(HttpMethod.Get, "/profile/banner");
 
 	public Task UpdateBannerAsync(IBrowserFile file) =>
 		api.CallAsync(HttpMethod.Post, "/profile/banner", data: file);
