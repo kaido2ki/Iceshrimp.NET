b976bd948985f5c25d2f4338a8f4f641b262300b
pancakes <trypancakes@tutanota.com>
2024-12-20 16:00:44 +1000

    [frontend/pages] Allow setting alt text for new or existing avatar and banner

--- Iceshrimp.Frontend/Core/ControllerModels/ProfileControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/ProfileControllerModel.cs
@@ -1,3 +1,4 @@
+using Iceshrimp.Frontend.Core.Miscellaneous;
 using Iceshrimp.Frontend.Core.Services;
 using Iceshrimp.Shared.Schemas.Web;
 using Microsoft.AspNetCore.Components.Forms;
@@ -9,14 +10,19 @@ internal class ProfileControllerModel(ApiClient api)
 	public Task<UserProfileEntity> GetProfileAsync() =>
 		api.CallAsync<UserProfileEntity>(HttpMethod.Get, "/profile");
 
-	public Task UpdateProfileAsync(UserProfileEntity request) =>
-		api.CallAsync(HttpMethod.Put, "/profile", data: request);
+	public Task UpdateProfileAsync(UserProfileEntity request, string? newAvatarAlt, string? newBannerAlt) =>
+		api.CallAsync(HttpMethod.Put, "/profile",
+		              QueryString.Create(new Dictionary<string, string?>
+		              {
+			              { "newAvatarAlt", newAvatarAlt }, { "newBannerAlt", newBannerAlt }
+		              }), request);
 
 	public Task<DriveFileResponse> GetAvatarAsync() =>
 		api.CallAsync<DriveFileResponse>(HttpMethod.Get, "/profile/avatar");
 
-	public Task UpdateAvatarAsync(IBrowserFile file) =>
-		api.CallAsync(HttpMethod.Post, "/profile/avatar", data: file);
+	public Task UpdateAvatarAsync(IBrowserFile file, string? altText) =>
+		api.CallAsync(HttpMethod.Post, "/profile/avatar",
+		              altText != null ? QueryString.Create("altText", altText) : QueryString.Empty, file);
 
 	public Task DeleteAvatarAsync() =>
 		api.CallAsync(HttpMethod.Delete, "/profile/avatar");
@@ -24,8 +30,9 @@ internal class ProfileControllerModel(ApiClient api)
 	public Task<DriveFileResponse> GetBannerAsync() =>
 		api.CallAsync<DriveFileResponse>(HttpMethod.Get, "/profile/banner");
 
-	public Task UpdateBannerAsync(IBrowserFile file) =>
-		api.CallAsync(HttpMethod.Post, "/profile/banner", data: file);
+	public Task UpdateBannerAsync(IBrowserFile file, string? altText) =>
+		api.CallAsync(HttpMethod.Post, "/profile/banner",
+		              altText != null ? QueryString.Create("altText", altText) : QueryString.Empty, file);
 
 	public Task DeleteBannerAsync() =>
 		api.CallAsync(HttpMethod.Delete, "/profile/banner");
