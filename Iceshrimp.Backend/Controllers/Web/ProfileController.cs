1ecd823cbe8f33a68509c6e31693c7c386f2b0a1
Laura Hausmann <laura@hausmann.dev>
2025-01-07 06:38:23 +0100

    [sln] Code cleanup

--- Iceshrimp.Backend/Controllers/Web/ProfileController.cs
+++ Iceshrimp.Backend/Controllers/Web/ProfileController.cs
@@ -2,7 +2,6 @@ using System.Net;
 using System.Net.Mime;
 using Iceshrimp.Backend.Controllers.Shared.Attributes;
 using Iceshrimp.Backend.Core.Database.Tables;
-using Iceshrimp.Backend.Core.Extensions;
 using Iceshrimp.Backend.Core.Middleware;
 using Iceshrimp.Backend.Core.Services;
 using Iceshrimp.Shared.Schemas.Web;
