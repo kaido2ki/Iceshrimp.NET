36111557866f82f59ec9262ebed7eda7a34e1fc1
Laura Hausmann <laura@hausmann.dev>
2025-01-08 18:09:12 +0100

    [backend/core] Increase TOTP window size to +-1

--- Iceshrimp.Backend/Core/Helpers/TotpHelper.cs
+++ Iceshrimp.Backend/Core/Helpers/TotpHelper.cs
@@ -4,8 +4,10 @@ namespace Iceshrimp.Backend.Core.Helpers;
 
 public static class TotpHelper
 {
+	private static readonly VerificationWindow VerificationWindow = new(1, 1);
+
 	public static bool Validate(string secret, string totp)
-		=> new Totp(Base32Encoding.ToBytes(secret)).VerifyTotp(totp, out _);
+		=> new Totp(Base32Encoding.ToBytes(secret)).VerifyTotp(totp, out _, VerificationWindow);
 
 	public static string GenerateSecret()
 		=> Base32Encoding.ToString(KeyGeneration.GenerateRandomKey());
