e0e8fe8b7225235d6be5184058b301d090281a3e
pancakes <trypancakes@tutanota.com>
2025-02-04 00:52:06 +1000

    [frontend/pages] Add upload and import actions to emoji management page

--- Iceshrimp.Frontend/Core/ControllerModels/EmojiControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/EmojiControllerModel.cs
@@ -1,3 +1,4 @@
+using Iceshrimp.Frontend.Core.Miscellaneous;
 using Iceshrimp.Frontend.Core.Services;
 using Iceshrimp.Shared.Schemas.Web;
 using Microsoft.AspNetCore.Components.Forms;
@@ -12,11 +13,15 @@ internal class EmojiControllerModel(ApiClient api)
 	public Task<List<EmojiResponse>> GetRemoteEmojiAsync() =>
 		api.CallAsync<List<EmojiResponse>>(HttpMethod.Get, "/emoji/remote");
 
-	public Task<EmojiResponse> UploadEmojiAsync(IBrowserFile file) =>
-		api.CallAsync<EmojiResponse>(HttpMethod.Post, "/emoji", data: file);
+	public Task<EmojiResponse> UploadEmojiAsync(IBrowserFile file, string? name) =>
+		api.CallAsync<EmojiResponse>(HttpMethod.Post, "/emoji",
+		                             name != null ? QueryString.Create("name", name) : QueryString.Empty, file);
 
 	public Task<EmojiResponse?> CloneEmojiAsync(string name, string host) =>
 		api.CallNullableAsync<EmojiResponse>(HttpMethod.Post, $"/emoji/clone/{name}@{host}");
+
+	public Task<bool> ImportEmojiAsync(IBrowserFile file) =>
+		api.CallNullableAsync(HttpMethod.Post, "/emoji/import", data: file);
 
 	public Task<EmojiResponse?> UpdateEmojiAsync(string id, UpdateEmojiRequest request) =>
 		api.CallNullableAsync<EmojiResponse>(HttpMethod.Patch, $"/emoji/{id}", data: request);
