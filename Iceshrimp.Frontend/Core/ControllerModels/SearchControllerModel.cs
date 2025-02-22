2a2754d80464d49caab17e46cbc8fd05553f97e0
pancakes <trypancakes@tutanota.com>
2025-02-22 11:53:44 +1000

    [frontend/components] Add global user dialog

--- Iceshrimp.Frontend/Core/ControllerModels/SearchControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/SearchControllerModel.cs
@@ -16,4 +16,11 @@ internal class SearchControllerModel(ApiClient api)
 
 	public Task<RedirectResponse?> LookupAsync(string target) =>
 		api.CallNullableAsync<RedirectResponse>(HttpMethod.Get, "/search/lookup", QueryString.Create("target", target));
+
+	public Task<List<UserResponse>> SearchAcctsAsync(string? username, string? host) =>
+		api.CallAsync<List<UserResponse>>(HttpMethod.Get, "/search/acct",
+		                                  (username != null
+			                                  ? QueryString.Create("username", username)
+			                                  : QueryString.Empty)
+		                                  + (host != null ? QueryString.Create("host", host) : QueryString.Empty));
 }
--- Iceshrimp.Frontend/Core/Services/GlobalComponentSvc.cs
+++ Iceshrimp.Frontend/Core/Services/GlobalComponentSvc.cs
@@ -10,4 +10,5 @@ public class GlobalComponentSvc
     public NoticeDialog?    NoticeDialog    { get; set; }
     public PromptDialog?    PromptDialog    { get; set; }
     public SelectDialog?    SelectDialog    { get; set; }
+    public UserDialog?      UserDialog      { get; set; }
 }
