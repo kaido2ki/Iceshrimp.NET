5976735af20185cc1b20305ffc61c78485494c67
pancakes <trypancakes@tutanota.com>
2025-02-26 18:14:29 +1000

    [frontend/components] Fallback to detecting attachment type by filename extension

---
+++ Iceshrimp.Frontend/Core/Miscellaneous/Constants.cs
@@ -0,0 +1,11 @@
+namespace Iceshrimp.Frontend.Core.Miscellaneous;
+
+public static class Constants
+{
+    public static readonly string[]
+        CommonImageExtensions = [".png", ".gif", ".jpeg", ".jpg", ".apng", ".bmp", ".tiff", ".avif", ".webp", ".jxl"];
+
+    public static readonly string[] CommonAudioExtensions = [".opus", ".ogg", ".m4a", ".mp3", ".flac", ".wav"];
+
+    public static readonly string[] CommonVideoExtensions = [".mov", ".mp4", ".webm", ".mkv"];
+}
