acef8b7945710ec58d354ad3cec689b978727194
pancakes <trypancakes@tutanota.com>
2025-03-09 00:25:37 +1000

    [frontend/components] Add paste listener for attachments and quotes

--- Iceshrimp.Frontend/Components/Compose.razor.js
+++ Iceshrimp.Frontend/Components/Compose.razor.js
@@ -13,3 +13,18 @@ export function getSelectionStart(element) {
 export function openUpload(element) {
     element.click();
 }
+
+export function setupPaste(dotnet, textarea, fileInput) {
+    textarea.addEventListener('paste', async (e) => {
+        e.preventDefault();
+
+        if (e.clipboardData.files.length === 0) {
+            const text = e.clipboardData.getData("text");
+            await dotnet.invokeMethodAsync("PasteQuote", text);
+        } else {
+            fileInput.files = e.clipboardData.files;
+            const changeEvent = new Event('change', {bubbles: true});
+            fileInput.dispatchEvent(changeEvent);
+        }
+    });
+}
