d4f133fd1a0e5596cf7639c9dd41c9d84657c3c1
Laura Hausmann <laura@hausmann.dev>
2025-04-09 15:27:26 +0200

    [backend] Code cleanup

--- Iceshrimp.Backend/Core/Federation/ActivityPub/UserRenderer.cs
+++ Iceshrimp.Backend/Core/Federation/ActivityPub/UserRenderer.cs
@@ -5,7 +5,6 @@ using Iceshrimp.Backend.Core.Database.Tables;
 using Iceshrimp.Backend.Core.Extensions;
 using Iceshrimp.Backend.Core.Federation.ActivityStreams.Types;
 using Iceshrimp.Backend.Core.Helpers.LibMfm.Conversion;
-using Iceshrimp.MfmSharp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
