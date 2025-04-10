9cee67431735279e958ef588084d0c44425a05ed
Lilian <lilian@blahaj.space>
2025-04-10 15:13:00 +0200

    [frontend/components] Allow Menu to use fixed position

--- Iceshrimp.Frontend/Components/Menu.razor.js
+++ Iceshrimp.Frontend/Components/Menu.razor.js
@@ -1,6 +1,6 @@
 export function getPosition(ref, scrollY){
     let rect = ref.getBoundingClientRect()
-    let x = rect.right - (rect.width / 2) + window.scrollX;
+    let x = rect.left + (rect.width / 2) + window.scrollX;
     let y = scrollY ? rect.bottom + window.scrollY : rect.bottom;
     return [x, y]
 }
