c145cde3626535d1d46b7c1a31a333a9d1dca69e
notfire <notfire@posteo.net>
2025-08-01 17:57:04 -0400

    [backend/cli] Ensure only local users can be affected by startup commands

--- Iceshrimp.Backend/Core/Extensions/WebApplicationExtensions.cs
+++ Iceshrimp.Backend/Core/Extensions/WebApplicationExtensions.cs
@@ -280,7 +280,9 @@ public static class WebApplicationExtensions
 
 				if (args[0] is "--create-admin-user")
 				{
-					await db.Users.Where(p => p.Username == username)
+					await db.Users
+					        .Where(p => p.Username == username)
+					        .Where(p => p.Host == null)
 					        .ExecuteUpdateAsync(p => p.SetProperty(i => i.IsAdmin, true));
 
 					app.Logger.LogInformation("Successfully created admin user.");
@@ -318,7 +320,9 @@ public static class WebApplicationExtensions
 
 			if (cmd is "--grant-admin")
 			{
-				var user = await db.Users.FirstOrDefaultAsync(p => p.UsernameLower == username.ToLowerInvariant());
+				var user = await db.Users
+				                   .Where(p => p.Host == null)
+				                   .FirstOrDefaultAsync(p => p.UsernameLower == username.ToLowerInvariant());
 				if (user == null)
 				{
 					app.Logger.LogError("User {username} not found.", username);
@@ -335,7 +339,9 @@ public static class WebApplicationExtensions
 
 			if (cmd is "--revoke-admin")
 			{
-				var user = await db.Users.FirstOrDefaultAsync(p => p.UsernameLower == username.ToLowerInvariant());
+				var user = await db.Users
+				                   .Where(p => p.Host == null)
+				                   .FirstOrDefaultAsync(p => p.UsernameLower == username.ToLowerInvariant());
 				if (user == null)
 				{
 					app.Logger.LogError("User {username} not found.", username);
