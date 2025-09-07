1b79c10c41f1b73b7592ca72467ebaf330790f13
pancakes <p@pancakes.gay>
2025-09-07 20:00:15 +1000

    [backend/masto-client] Fix URLs in OAuthAuthorizationServerResponse

--- Iceshrimp.Backend/Controllers/Mastodon/Schemas/OAuthAuthorizationServerResponse.cs
+++ Iceshrimp.Backend/Controllers/Mastodon/Schemas/OAuthAuthorizationServerResponse.cs
@@ -5,13 +5,13 @@ namespace Iceshrimp.Backend.Controllers.Mastodon.Schemas;
 
 public class OAuthAuthorizationServerResponse(string webDomain)
 {
-    [J("issuer")] public string Issuer => webDomain;
+    [J("issuer")] public string Issuer => $"https://{webDomain}";
 
-    [J("authorization_endpoint")] public string AuthorizationEndpoint => $"{webDomain}/oauth/authorize";
+    [J("authorization_endpoint")] public string AuthorizationEndpoint => $"https://{webDomain}/oauth/authorize";
 
-    [J("token_endpoint")] public string TokenEndpoint => $"{webDomain}/oauth/token";
+    [J("token_endpoint")] public string TokenEndpoint => $"https://{webDomain}/oauth/token";
 
-    [J("app_registration_endpoint")] public string AppRegistrationEndpoint => $"{webDomain}/api/v1/apps";
+    [J("app_registration_endpoint")] public string AppRegistrationEndpoint => $"https://{webDomain}/api/v1/apps";
 
     [J("scopes_supported")] public List<string> ScopesSupported => MastodonOauthHelpers.AllScopes;
 
@@ -24,11 +24,11 @@ public class OAuthAuthorizationServerResponse(string webDomain)
     [J("token_endpoint_auth_methods_supported")]
     public List<string> TokenEndpointAuthMethodsSupported => ["client_secret_post"];
 
-    [J("service_documentation")] public string ServiceDocumentation => "https://docs.joinmastodon.org/";
+    [J("service_documentation")] public string ServiceDocumentation => $"https://{webDomain}/openapi/";
 
     [J("ui_locales_supported")] public List<string> UiLocalesSupported => ["en"];
 
-    [J("revocation_endpoint")] public string RevocationEndpoint => $"{webDomain}/oauth/revoke";
+    [J("revocation_endpoint")] public string RevocationEndpoint => $"https://{webDomain}/oauth/revoke";
 
     [J("revocation_endpoint_auth_methods_supported")]
     public List<string> RevocationEndpointAuthMethodsSupported => ["client_secret_post"];
