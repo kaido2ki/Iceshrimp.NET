390dc0f24ab8ae002d151483bf66fa4d68eeec09
pancakes <p@pancakes.gay>
2025-09-23 18:28:53 +1000

    [frontend/pages] Add moderation page for managing reports

--- Iceshrimp.Frontend/Core/ControllerModels/ModerationControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/ModerationControllerModel.cs
@@ -6,11 +6,20 @@ namespace Iceshrimp.Frontend.Core.ControllerModels;
 
 internal class ModerationControllerModel(ApiClient api)
 {
+    public Task SuspendUserAsync(string id) =>
+        api.CallNullableAsync(HttpMethod.Post, $"/moderation/users/{id}/suspend");
+
     [LinkPagination(20, 40)]
     public Task<List<ReportResponse>> GetReportsAsync(PaginationQuery pq, bool resolved = false) =>
         api.CallAsync<List<ReportResponse>>(HttpMethod.Get, "/moderation/reports",
                                             pq + QueryString.Create("resolved", resolved ? "true" : "false"));
 
+    public Task<ReportResponse?> GetReportAsync(string id) =>
+        api.CallNullableAsync<ReportResponse>(HttpMethod.Get, $"/moderation/reports/{id}");
+
     public Task ResolveReportAsync(string id) =>
         api.CallAsync(HttpMethod.Post, $"/moderation/reports/{id}/resolve");
+
+    public Task ForwardReportAsync(string id) =>
+        api.CallAsync(HttpMethod.Post, $"/moderation/reports/{id}/forward");
 }
