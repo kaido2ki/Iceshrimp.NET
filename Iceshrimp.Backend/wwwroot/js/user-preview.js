cb21b44844e990d2b4b000d5fb8b4c960b6622c7
pancakes <p@pancakes.gay>
2025-11-17 17:25:08 +1000

    [backend/razor] Use button style links for pagination in profile public preview

--- Iceshrimp.Backend/wwwroot/js/user-preview.js
+++
@@ -1,7 +0,0 @@
-function navigate(event) {
-    const target = event.target.getAttribute('data-target')
-    if (event.ctrlKey || event.metaKey)
-        window.open(target, '_blank');
-    else
-        window.location.href = target;
-}
