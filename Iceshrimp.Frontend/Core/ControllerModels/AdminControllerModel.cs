670b79142e9707098f1f592d9feb1c2e9a5481d8
pancakes <p@pancakes.gay>
2025-11-25 17:48:30 +1000

    [frontend/pages] Fix about/debug page for remote users

--- Iceshrimp.Frontend/Core/ControllerModels/AdminControllerModel.cs
+++ Iceshrimp.Frontend/Core/ControllerModels/AdminControllerModel.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Nodes;
+using Iceshrimp.Frontend.Core.Miscellaneous;
 using Iceshrimp.Frontend.Core.Services;
 using Iceshrimp.Shared.Schemas.Web;
 
@@ -14,6 +15,14 @@ internal class AdminControllerModel(ApiClient api)
 
 	public Task<JsonObject?> GetActivityByUserIdAsync(string id) =>
 		api.CallNullableAsync<JsonObject>(HttpMethod.Get, $"/admin/activities/users/{id}");
+
+	public Task<JsonObject?> GetActivityAsync(string uri, string? userId)
+	{
+		var query = new QueryString();
+		query.Add("uri", uri);
+		if (userId != null) query.Add("userId", userId);
+		return api.CallNullableAsync<JsonObject>(HttpMethod.Get, $"/admin/activities/fetch", query);
+	}
 
 	//TODO: other endpoints
 }
