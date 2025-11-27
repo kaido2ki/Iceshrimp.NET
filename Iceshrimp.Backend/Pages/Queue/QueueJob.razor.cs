b63c54ae2412bb005b760d5c7c1506e4d33c84c3
pancakes <p@pancakes.gay>
2025-11-27 20:32:04 +1000

    [backend/razor] Port Queue Job page to .razor

---
+++ Iceshrimp.Backend/Pages/Queue/QueueJob.razor.cs
@@ -0,0 +1,29 @@
+using Iceshrimp.Backend.Components.Helpers;
+using Iceshrimp.Backend.Core.Database;
+using Iceshrimp.Backend.Core.Database.Tables;
+using Iceshrimp.Backend.Core.Middleware;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iceshrimp.Backend.Pages.Queue;
+
+public partial class QueueJob(DatabaseContext db) : AdminComponentBase
+{
+    [Parameter] public required Guid Id { get; set; }
+
+    private Job JobDetails { get; set; } = null!;
+
+    public static Dictionary<string, string> Lookup = new()
+    {
+        ["inbox"]       = "body",
+        ["deliver"]     = "payload",
+        ["pre-deliver"] = "serializedActivity"
+    };
+
+    protected override async Task OnInitializedAsync()
+    {
+        JobDetails = await db.Jobs.FirstOrDefaultAsync(p => p.Id == Id) ??
+                     throw GracefulException.NotFound($"Job {Id} not found");
+    }
+}
+
