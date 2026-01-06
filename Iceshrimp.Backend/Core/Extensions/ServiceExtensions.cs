04c7a3b50448b0d88012d1634d6fad817c35b17a
pancakes <p@pancakes.gay>
2026-01-06 14:50:59 +1000

    [backend/core] Enable OpenAPI XML documentation comment generation

--- Iceshrimp.Backend/Core/Extensions/ServiceExtensions.cs
+++ Iceshrimp.Backend/Core/Extensions/ServiceExtensions.cs
@@ -102,6 +102,8 @@ public static class ServiceExtensions
 				                              });
 
 				options.AddFilters();
+
+				options.IncludeXmlComments(Assembly.GetExecutingAssembly());
 			});
 		}
 
