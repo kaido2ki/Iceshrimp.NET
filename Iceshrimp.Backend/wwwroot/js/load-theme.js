5bc16dc4b44b064854a72f180519729f0643298e
pancakes <p@pancakes.gay>
2026-06-02 21:35:03 +1000

    [backend/pages] Fix typo

--- Iceshrimp.Backend/wwwroot/js/load-theme.js
+++ Iceshrimp.Backend/wwwroot/js/load-theme.js
@@ -1,5 +1,5 @@
 onload = (ev) => {
     const themePrefs = window.localStorage.getItem("theme");
     if (themePrefs === "light") document.body.classList.add("theme-light");
-    else if (themePrefs === "dark") document.body.classList.add("theme-light");
+    else if (themePrefs === "dark") document.body.classList.add("theme-dark");
 };
