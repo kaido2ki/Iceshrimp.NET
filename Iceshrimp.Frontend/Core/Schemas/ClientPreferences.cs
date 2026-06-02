7f7889bdda339224d16098919edd70968af53c0a
pancakes <p@pancakes.gay>
2026-06-02 21:17:36 +1000

    [backend/pages] Move FE theme logic to backend to reduce flashing and load in all contexts

--- Iceshrimp.Frontend/Core/Schemas/ClientPreferences.cs
+++ Iceshrimp.Frontend/Core/Schemas/ClientPreferences.cs
@@ -3,16 +3,8 @@ namespace Iceshrimp.Frontend.Core.Schemas;
 // NOTE: All client preferences must have a sensible default value
 public class ClientPreferences
 {
-    public ClientTheme             Theme         { get; set; } = ClientTheme.System;
     public string                  CustomCss     { get; set; } = "";
     public NotificationPreferences Notifications { get; set; } = new NotificationPreferences();
-}
-
-public enum ClientTheme
-{
-    System,
-    Light,
-    Dark,
 }
 
 public class NotificationPreferences
