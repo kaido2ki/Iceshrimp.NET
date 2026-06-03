3bab8c2ccb06d0e5fb0adf0c2ff0d6c58b685aa8
notfire <notfire@posteo.net>
2026-06-03 18:34:07 -0400

    [backend/masto-api] Use precomputed reactions count instead of recalculating it

--- Iceshrimp.Backend/Controllers/Mastodon/Renderers/NoteRenderer.cs
+++ Iceshrimp.Backend/Controllers/Mastodon/Renderers/NoteRenderer.cs
@@ -270,7 +270,7 @@ public class NoteRenderer(
 			RepliesCount     = note.RepliesCount,
 			RenoteCount      = note.RenoteCount,
 			FavoriteCount    = note.LikeCount,
-			ReactionsCount   = reactions.Select(p => p.AccountIds?.Count).Sum() ?? 0,
+			ReactionsCount   = reactions.Select(p => p.Count).Sum(),
 			IsFavorited      = liked,
 			IsRenoted        = renoted,
 			IsBookmarked     = bookmarked,
