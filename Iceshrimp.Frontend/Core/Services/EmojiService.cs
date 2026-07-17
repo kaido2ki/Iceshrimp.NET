4978a89ca6a080b6c909436ed35e64925ee682a9
pancakes <p@pancakes.gay>
2026-07-17 22:10:51 +1000

    [frontend/components] Deduplicate use of emojis from EmojiService

--- Iceshrimp.Frontend/Core/Services/EmojiService.cs
+++ Iceshrimp.Frontend/Core/Services/EmojiService.cs
@@ -10,27 +10,26 @@ internal class EmojiService(ApiService api, GlobalComponentSvc global)
 {
 	[Inject] private ApiService           Api    { get; set; } = api;
 	[Inject] private GlobalComponentSvc   Global { get; set; } = global;
-	private          List<EmojiResponse>? Emojis { get; set; }
+	
+	/// <summary>
+	/// List of custom emojis for the instance. Call <see cref="GetEmojiAsync"/> before getting this.
+	/// </summary>
+	public List<EmojiResponse>? Emojis { get; set; }
 
-	public async Task<List<EmojiResponse>> GetEmojiAsync()
+	/// <summary>
+	/// Initialize the <see cref="Emojis"/> list for the session. If the list is already populated this function is a no-op. It is safe to call this multiple times per session.
+	/// </summary>
+	public async Task GetEmojiAsync()
 	{
-		if (Emojis is not null) return Emojis;
+		if (Emojis != null) return;
 		try
 		{
-			var emoji = await Api.Emoji.GetAllEmojiAsync();
-			Emojis = emoji;
-			return Emojis;
+			Emojis = await Api.Emoji.GetAllEmojiAsync();
 		}
 		catch (ApiException e)
 		{
 			await Global.NoticeDialog?.DisplayApiError(e)!;
-			return [];
-		}
-		catch (JsonException)
-		{
-			await Global.NoticeDialog?.Display("Emojis provided by the server didn't match what the client expected",
-			                                   NoticeDialog.NoticeType.Error)!;
-			return [];
+			Emojis = [];
 		}
 	}
 }
