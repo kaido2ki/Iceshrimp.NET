a6d4c99d96fff79f9928cec0874a08a8403021bb
pancakes <p@pancakes.gay>
2026-09-05 16:53:46 +1000

    [frontend/components] Clean up unused JS

--- Iceshrimp.Frontend/Components/Menu.razor.js
+++
@@ -1,6 +0,0 @@
-export function getPosition(ref, scrollY){
-    let rect = ref.getBoundingClientRect()
-    let x = rect.left + (rect.width / 2) + window.scrollX;
-    let y = scrollY ? rect.bottom + window.scrollY : rect.bottom;
-    return [x, y]
-}
